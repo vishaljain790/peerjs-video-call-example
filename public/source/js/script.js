491be779d1211c843c1f5f661557df6bde449b3a
Vishal_team <Vishal.j@myteam11.com>
2018-08-09 17:38:24 +0530

    third commit

--- public/source/js/script.js
+++ public/source/js/script.js
@@ -19,13 +19,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
         debug: 3,
         config: {
             'iceServers': [{
-                    url: 'stun:stun.l.google.com:19302'
+
+                    url: 'stun:stun1.l.google.com:19302'
                 },
                 {
-                    url: 'turn:homeo@turn.bistri.com:80',
-                    credential: 'homeo'
+                    url: 'turn:numb.viagenie.ca',
+                    credential: 'muazkh',
+                    username: 'webrtc@live.com'
+                },
+                {
+                    url: 'turn:numb.viagenie.ca',
+                    credential: 'muazkh',
+                    username: 'webrtc@live.com'
+                },
+                {
+                    url: 'turn:192.158.29.39:3478?transport=udp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
+                },
+                {
+                    url: 'turn:192.158.29.39:3478?transport=tcp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
                 }
             ]
+
+
         }
     });
 
