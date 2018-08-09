a2ded54210d3ab134ff8ea6b5979b2c9f7227bc5
Vishal_team <Vishal.j@myteam11.com>
2018-08-09 17:57:59 +0530

    modified

--- public/source/js/script.js
+++ public/source/js/script.js
@@ -16,36 +16,36 @@ document.addEventListener("DOMContentLoaded", function (event) {
         host: location.hostname,
         port: location.port || (location.protocol === 'https:' ? 443 : 80),
         path: '/peerjs',
-        debug: 3,
-        config: {
-            'iceServers': [{
+        debug: 3
+        // config: {
+        //     'iceServers': [{
 
-                    url: 'stun:stun1.l.google.com:19302'
-                },
-                {
-                    url: 'turn:numb.viagenie.ca',
-                    credential: 'muazkh',
-                    username: 'webrtc@live.com'
-                },
-                {
-                    url: 'turn:numb.viagenie.ca',
-                    credential: 'muazkh',
-                    username: 'webrtc@live.com'
-                },
-                {
-                    url: 'turn:192.158.29.39:3478?transport=udp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
-                },
-                {
-                    url: 'turn:192.158.29.39:3478?transport=tcp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
-                }
-            ]
+        //             url: 'stun:stun1.l.google.com:19302'
+        //         },
+        //         {
+        //             url: 'turn:numb.viagenie.ca',
+        //             credential: 'muazkh',
+        //             username: 'webrtc@live.com'
+        //         },
+        //         {
+        //             url: 'turn:numb.viagenie.ca',
+        //             credential: 'muazkh',
+        //             username: 'webrtc@live.com'
+        //         },
+        //         {
+        //             url: 'turn:192.158.29.39:3478?transport=udp',
+        //             credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        //             username: '28224511:1379330808'
+        //         },
+        //         {
+        //             url: 'turn:192.158.29.39:3478?transport=tcp',
+        //             credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        //             username: '28224511:1379330808'
+        //         }
+        //     ]
 
 
-        }
+        // }
     });
 
 
